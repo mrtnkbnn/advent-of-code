09ef478545357077b5c0aa6e54a8a07095457db6
Martin Kabanen <martin.kabanen@gmail.com>
2022-12-02 17:50:05 +0200

    fix: 2022 day 01

--- 2022/01.cpp
+++ 2022/01.cpp
@@ -4,22 +4,19 @@ using namespace std;
 int main() {
     ifstream input("inputs/01.in");
     string line;
-    set<int> calories;
+    vector<int> calories;
     int cur = 0;
     while (getline(input, line)) {
         if (line.empty()) {
-            calories.insert(cur);
+            calories.push_back(cur);
             cur = 0;
         } else {
             cur += stoi(line);
         }
     }
-    calories.insert(cur);
-    auto it = --calories.end();
-    cout << "Part 1: " << *it << endl;
-    int total = *it;
-    total += *(--it);
-    total += *(--it);
-    cout << "Part 2: " << total << endl;
+    calories.push_back(cur);
+    sort(calories.begin(), calories.end(), greater());
+    cout << "Part 1: " << calories[0] << endl;
+    cout << "Part 2: " << calories[0] + calories[1] + calories[2] << endl;
     return 0;
 }

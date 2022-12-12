3bb113737249e0a271e36865f7dd0cc77faeda75
Martin Kabanen <martin.kabanen@gmail.com>
2022-12-12 21:05:53 +0200

    refactor: 2022 day 12 use priority queue

--- 2022/12.cpp
+++ 2022/12.cpp
@@ -1,6 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct pos {
+    int row, col, dist;
+
+    pos(int _row, int _col, int _dist) {
+        row = _row;
+        col = _col;
+        dist = _dist;
+    }
+};
+
+struct comparator {
+    bool operator ()(pos* pos1, pos* pos2) {
+        return pos1->dist > pos2->dist || (pos1->dist == pos2->dist && (pos1->row > pos2->row || (pos1->row == pos2->row && pos1->col > pos2->col)));
+    }
+};
+
 int main() {
     string line;
     int ROWS, COLS, startRow, startCol, endRow, endCol;
@@ -29,36 +45,30 @@ int main() {
     ROWS = hill.size();
     vector<vector<int>> dist(ROWS, vector<int>(COLS, INT_MAX));
     vector<vector<bool>> visited(ROWS, vector<bool>(COLS));
+    priority_queue<pos*, vector<pos*>, comparator> Q;
     dist[endRow][endCol] = 0;
-    while (true) {
-        int minDist = INT_MAX, curRow, curCol;
-        for (int row = 0; row < ROWS; ++row) {
-            for (int col = 0; col < COLS; ++col) {
-                if (!visited[row][col]) {
-                    int curDist = dist[row][col];
-                    if (curDist < minDist) {
-                        minDist = curDist;
-                        curRow = row;
-                        curCol = col;
-                    }
-                }
-            }
-        }
-        if (minDist == INT_MAX) {
-            break;
-        }
+    Q.push(new pos(endRow, endCol, 0));
+    while (!Q.empty()) {
+        pos* curPos = Q.top();
+        Q.pop();
+        int curRow = curPos->row;
+        int curCol = curPos->col;
         char current = hill[curRow][curCol];
         if (curRow - 1 >= 0 && !visited[curRow - 1][curCol] && hill[curRow - 1][curCol] >= current - 1 && dist[curRow][curCol] + 1 < dist[curRow - 1][curCol]) {
             dist[curRow - 1][curCol] = dist[curRow][curCol] + 1;
+            Q.push(new pos(curRow - 1, curCol, dist[curRow][curCol] + 1));
         }
         if (curRow + 1 < ROWS && !visited[curRow + 1][curCol] && hill[curRow + 1][curCol] >= current - 1 && dist[curRow][curCol] + 1 < dist[curRow + 1][curCol]) {
             dist[curRow + 1][curCol] = dist[curRow][curCol] + 1;
+            Q.push(new pos(curRow + 1, curCol, dist[curRow][curCol] + 1));
         }
         if (curCol - 1 >= 0 && !visited[curRow][curCol - 1] && hill[curRow][curCol - 1] >= current - 1 && dist[curRow][curCol] + 1 < dist[curRow][curCol - 1]) {
             dist[curRow][curCol - 1] = dist[curRow][curCol] + 1;
+            Q.push(new pos(curRow, curCol - 1, dist[curRow][curCol] + 1));
         }
         if (curCol + 1 < COLS && !visited[curRow][curCol + 1] && hill[curRow][curCol + 1] >= current - 1 && dist[curRow][curCol] + 1 < dist[curRow][curCol + 1]) {
             dist[curRow][curCol + 1] = dist[curRow][curCol] + 1;
+            Q.push(new pos(curRow, curCol + 1, dist[curRow][curCol] + 1));
         }
         visited[curRow][curCol] = true;
     }

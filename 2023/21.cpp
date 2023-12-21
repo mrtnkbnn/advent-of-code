91b2e067a1f6145e726902b7c4bedae261fe4b0f
Martin Kabanen <martin.kabanen@gmail.com>
2023-12-21 21:51:15 +0200

    2023-21 part 2

--- 2023/21.cpp
+++ 2023/21.cpp
@@ -3,7 +3,7 @@ using namespace std;
 using num = long long;
 
 int main() {
-    num rows = 0, part1 = 0, part2 = 0;
+    num rows = 0, part1 = 0;
     string line;
     pair<num, num> startPosition;
     vector<vector<char>> garden;
@@ -26,12 +26,18 @@ int main() {
     vector<pair<num, num>> sides = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
     queue<pair<pair<num, num>, num>> q;
     q.push({startPosition, 0});
-    num stepsLimit = 64;
+    num even = 0, odd = 0, evenCorners = 0, oddCorners = 0;
     while (!q.empty()) {
         pair<pair<num, num>, num> current = q.front();
         q.pop();
-        if (current.second <= stepsLimit && current.second % 2 == stepsLimit % 2) ++part1;
-        if (current.second > stepsLimit) continue;
+        if (current.second % 2 == 0) {
+            ++even;
+            if (current.second < 65) ++part1;
+            if (current.second > 65) ++evenCorners;
+        } else {
+            ++odd;
+            if (current.second > 65) ++oddCorners;
+        }
         pair<num, num> position = current.first;
         for (pair<num, num> &side : sides) {
             pair<num, num> next = {position.first + side.first, position.second + side.second};
@@ -41,6 +47,8 @@ int main() {
             }
         }
     }
+    num n = 202300;
     cout << "Part 1: " << part1 << endl;
+    cout << "Part 2: " << ((n + 1) * (n + 1) * odd) + (n * n * even) - ((n + 1) * oddCorners) + (n * evenCorners) << endl;
     return 0;
 }

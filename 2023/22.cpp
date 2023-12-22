96ea3a2dc46c584f6c1bca48940583669a1f41a8
Martin Kabanen <martin.kabanen@gmail.com>
2023-12-22 18:47:01 +0200

    2023-22 part 2

--- 2023/22.cpp
+++ 2023/22.cpp
@@ -39,8 +39,40 @@ bool overlap(pair<num, num> &coord1, pair<num, num> &coord2) {
     return true;
 }
 
+num fall(num brickName, vector<Brick> bricks) {
+    num result = 0;
+    for (num i = 0; i < bricks.size(); ++i) {
+        if (bricks[i].name == brickName) {
+            bricks.erase(bricks.begin() + i);
+            break;
+        }
+    }
+    for (num i = 0; i < bricks.size(); ++i) {
+        if (bricks[i].z.first == 1) continue;
+        num z = 0;
+        vector<num> restsOn;
+        for (num j = i - 1; j >= 0; --j) {
+            if (overlap(bricks[i].x, bricks[j].x) && overlap(bricks[i].y, bricks[j].y)) {
+                if (bricks[j].z.second > z) {
+                    z = bricks[j].z.second;
+                    restsOn = {bricks[j].name};
+                } else if (bricks[j].z.second == z) {
+                    restsOn.push_back(bricks[j].name);
+                }
+            }
+        }
+        ++z;
+        num delta = abs(z - bricks[i].z.first);
+        if (delta > 0) ++result;
+        bricks[i].z.first -= delta;
+        bricks[i].z.second -= delta;
+        bricks[i].restsOn = restsOn;
+    }
+    return result;
+}
+
 int main() {
-    num x1, y1, z1, x2, y2, z2, names = 0;
+    num x1, y1, z1, x2, y2, z2, names = 0, part2 = 0;
     vector<Brick> bricks;
     while (scanf("%lld,%lld,%lld~%lld,%lld,%lld\n", &x1, &y1, &z1, &x2, &y2, &z2) != EOF) {
         bricks.push_back(Brick(names, x1, y1, z1, x2, y2, z2));
@@ -48,8 +80,7 @@ int main() {
     }
     sort(bricks.begin(), bricks.end(), byHeight);
     for (num i = 0; i < bricks.size(); ++i) {
-        Brick brick = bricks[i];
-        if (brick.z.first == 1) continue;
+        if (bricks[i].z.first == 1) continue;
         num z = 0;
         vector<num> restsOn;
         for (num j = i - 1; j >= 0; --j) {
@@ -72,6 +103,8 @@ int main() {
     for (num i = 0; i < bricks.size(); ++i) {
         if (bricks[i].restsOn.size() == 1) cannot.insert(bricks[i].restsOn[0]);
     }
+    for (num n : cannot) part2 += fall(n, bricks);
     cout << "Part 1: " << bricks.size() - cannot.size() << endl;
+    cout << "Part 2: " << part2 << endl;
     return 0;
 }

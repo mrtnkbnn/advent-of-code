15faca7e0da3ebe478078b3275241f57173869ac
Martin Kabanen <martin.kabanen@gmail.com>
2024-12-03 19:08:35 +0200

    improve 2024-03

--- 2024/03.cpp
+++ 2024/03.cpp
@@ -4,19 +4,17 @@ using num = long long;
 
 int main() {
     num part1 = 0, part2 = 0;
-    regex r("mul\\(\\d+,\\d+\\)|do\\(\\)|don't\\(\\)");
+    regex r("mul\\((\\d+),(\\d+)\\)|do(n't)?\\(\\)");
     string memory;
     bool enabled = true;
     while (getline(cin, memory)) {
-        for (sregex_iterator i = sregex_iterator(memory.begin(), memory.end(), r); i != sregex_iterator(); ++i) {
+        for (auto i = sregex_iterator(memory.begin(), memory.end(), r); i != sregex_iterator(); ++i) {
             smatch match = *i;
-            if (match.str() == "do()") {
-                enabled = true;
-            } else if (match.str() == "don't()") {
-                enabled = false;
-            } else {
-                num left = stoll(match.str().substr(4, match.str().find(",") - 4));
-                num right = stoll(match.str().substr(match.str().find(",") + 1, match.str().find(")") - match.str().find(",") - 1));
+            if (match.str() == "do()") enabled = true;
+            else if (match.str() == "don't()") enabled = false;
+            else {
+                num left = stoll(match[1].str());
+                num right = stoll(match[2].str());
                 part1 += left * right;
                 if (enabled) part2 += left * right;
             }

b10142aa3b311b5619a184e1735a86a88555aafa
Martin Kabanen <martin.kabanen@gmail.com>
2024-12-06 12:58:46 +0200

    improve 2024-06

--- 2024/06.cpp
+++ 2024/06.cpp
@@ -1,76 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
 using num = long long;
+using coord = pair<num, num>;
+vector<coord> dirs = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
 
-bool loops(vector<vector<char>> map, pair<num, num> &start, pair<num, num> &pos, num rows, num cols) {
-    map[pos.first][pos.second] = '#';
-    set<pair<num, num>> seen;
-    set<tuple<num, num, num>> mem;
-    vector<pair<num, num>> dirs = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-    num dir = 0;
-    queue<pair<num, num>> q;
-    q.push(start);
-    while (!q.empty()) {
-        pair<num, num> cur = q.front();
-        q.pop();
+bool loops(vector<vector<char>> grid, set<tuple<num, num, num>> mem, coord cur, num dir, coord next, num rows, num cols) {
+    grid[next.first][next.second] = '#';
+    mem.erase({cur.first, cur.second, dir});
+    while (true) {
         if (mem.find({cur.first, cur.second, dir}) != mem.end()) return true;
-        seen.insert(cur);
         mem.insert({cur.first, cur.second, dir});
-        pair<num, num> next = {cur.first + dirs[dir].first, cur.second + dirs[dir].second};
-        if (next.first < 0 || next.second < 0 || next.first >= rows || next.second >= cols) {
-            return false;
-        } else if (map[next.first][next.second] == '#') {
+        coord next = {cur.first + dirs[dir].first, cur.second + dirs[dir].second};
+        if (next.first < 0 || next.second < 0 || next.first >= rows || next.second >= cols) return false;
+        else if (grid[next.first][next.second] == '#') {
             ++dir;
             if (dir == 4) dir = 0;
-            q.push(cur);
         } else {
-            q.push(next);
+            cur = next;
         }
     }
     return false;
 }
 
 int main() {
-    num part1 = 0, part2 = 0, r = 0;
+    num part2 = 0, r = 0;
     string line;
-    vector<vector<char>> map;
-    pair<num, num> start;
+    vector<vector<char>> grid;
+    coord start;
     while (getline(cin, line)) {
         vector<char> row;
         for (num i = 0; i < line.length(); ++i) {
             row.push_back(line[i]);
             if (line[i] != '.' && line[i] != '#') start = {r, i};
         }
-        map.push_back(row);
+        grid.push_back(row);
         ++r;
     }
-    num rows = map.size();
-    num cols = map[0].size();
-    set<pair<num, num>> seen;
-    vector<pair<num, num>> dirs = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+    num rows = grid.size();
+    num cols = grid[0].size();
+    set<coord> seen;
+    set<tuple<num, num, num>> mem;
     num dir = 0;
-    queue<pair<num, num>> q;
-    q.push(start);
-    while (!q.empty()) {
-        pair<num, num> cur = q.front();
-        q.pop();
+    coord cur = start;
+    while (true) {
         seen.insert(cur);
-        pair<num, num> next = {cur.first + dirs[dir].first, cur.second + dirs[dir].second};
-        if (next.first < 0 || next.second < 0 || next.first >= rows || next.second >= cols) {
-            break;
-        } else if (map[next.first][next.second] == '#') {
+        coord next = {cur.first + dirs[dir].first, cur.second + dirs[dir].second};
+        if (next.first < 0 || next.second < 0 || next.first >= rows || next.second >= cols) break;
+        else if (grid[next.first][next.second] == '#') {
             ++dir;
             if (dir == 4) dir = 0;
-            q.push(cur);
         } else {
-            q.push(next);
+            if (seen.find(next) == seen.end() && loops(grid, mem, cur, dir, next, rows, cols)) ++part2;
+            cur = next;
         }
     }
-    part1 = seen.size();
-    for (auto pos : seen) {
-        if (loops(map, start, pos, rows, cols)) ++part2;
-    }
-    cout << "Part 1: " << part1 << endl;
+    cout << "Part 1: " << seen.size() << endl;
     cout << "Part 2: " << part2 << endl;
     return 0;
 }

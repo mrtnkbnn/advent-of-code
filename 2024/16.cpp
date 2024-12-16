5eaa2abe2694ed83394f1a9becb39bc36daac35d
Martin Kabanen <martin.kabanen@gmail.com>
2024-12-16 22:14:30 +0200

    improve 2024-16

--- 2024/16.cpp
+++ 2024/16.cpp
@@ -4,52 +4,47 @@ using num = long long;
 using coord = pair<num, num>;
 const vector<coord> dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
-vector<vector<num>> dijkstra(coord start, coord end, coord startDir, vector<string> &grid) {
-    num rows = grid.size(), cols = grid[0].length();
-    vector<vector<bool>> visited(rows, vector<bool>(cols, false));
-    vector<vector<num>> distance(rows, vector<num>(cols, LONG_LONG_MAX));
-    vector<vector<coord>> dir(rows, vector<coord>(cols, {0, 0}));
-    priority_queue<pair<num, coord>, vector<pair<num, coord>>, greater<pair<num, coord>>> q;
-    q.push({0, start});
-    distance[start.first][start.second] = 0;
-    dir[start.first][start.second] = startDir;
-    while (!q.empty()) {
-        coord cur = q.top().second;
-        q.pop();
-        if (visited[cur.first][cur.second]) continue;
-        visited[cur.first][cur.second] = true;
-        for (coord delta : dirs) {
-            coord curDir = dir[cur.first][cur.second];
-            if ((curDir.first == delta.first && curDir.second != delta.second) || (curDir.second == delta.second && curDir.first != delta.first)) continue;
-            coord next = {cur.first + delta.first, cur.second + delta.second};
-            if (grid[next.first][next.second] == '#') continue;
-            num rotate = delta == curDir ? 0 : 1000;
-            if (distance[next.first][next.second] > distance[cur.first][cur.second] + 1 + rotate) {
-                distance[next.first][next.second] = distance[cur.first][cur.second] + 1 + rotate;
-                dir[next.first][next.second] = delta;
-                if (next != end) q.push({distance[cur.first][cur.second] + 1 + rotate, next});
-            }
-        }
-    }
-    return distance;
-}
-
 int main() {
-    num part1 = 0, part2 = 0;
+    num part1 = LONG_LONG_MAX, part2 = 0;
     string line;
     vector<string> grid;
     while (getline(cin, line)) grid.push_back(line);
     num rows = grid.size(), cols = grid[0].length();
-    auto distance1 = dijkstra({rows - 2, 1}, {1, cols - 2}, {0, 1}, grid);
-    auto distance2 = dijkstra({1, cols - 2}, {rows - 2, 1}, {0, -1}, grid);
-    auto distance3 = dijkstra({1, cols - 2}, {rows - 2, 1}, {1, 0}, grid);
-    for (num row = 0; row < rows; ++row) {
-        for (num col = 0; col < cols; ++col) {
-            if (distance1[row][col] + distance2[row][col] == distance1[1][cols - 2]) ++part2;
-            else if (distance1[row][col] + distance3[row][col] == distance1[1][cols - 2]) ++part2;
+    priority_queue<tuple<num, coord, coord, set<coord>>, vector<tuple<num, coord, coord, set<coord>>>, greater<tuple<num, coord, coord, set<coord>>>> q;
+    vector<vector<map<coord, num>>> distance(rows, vector<map<coord, num>>(cols, {{{1, 0}, LONG_LONG_MAX}, {{-1, 0}, LONG_LONG_MAX}, {{0, 1}, LONG_LONG_MAX}, {{0, -1}, LONG_LONG_MAX}}));
+    vector<vector<map<coord, set<coord>>>> path(rows, vector<map<coord, set<coord>>>(cols, {{{1, 0}, {}}, {{-1, 0}, {}}, {{0, 1}, {}}, {{0, -1}, {}}}));
+    q.push({0, {rows - 2, 1}, {0, 1}, {{rows - 2, 1}}});
+    distance[rows - 2][1][{0, 1}] = 0;
+    while (!q.empty()) {
+        auto top = q.top();
+        q.pop();
+        num curDist = get<0>(top);
+        coord cur = get<1>(top);
+        coord curDir = get<2>(top);
+        set<coord> curPath = get<3>(top);
+        for (coord dir : dirs) {
+            if ((curDir.first == dir.first && curDir.second != dir.second) || (curDir.second == dir.second && curDir.first != dir.first)) continue;
+            num rotate = dir == curDir ? 0 : 1000;
+            coord nextPos = {cur.first + dir.first, cur.second + dir.second};
+            if (grid[nextPos.first][nextPos.second] == '#') continue;
+            if (distance[nextPos.first][nextPos.second][dir] > distance[cur.first][cur.second][curDir] + 1 + rotate) {
+                distance[nextPos.first][nextPos.second][dir] = distance[cur.first][cur.second][curDir] + 1 + rotate;
+                path[nextPos.first][nextPos.second][dir] = path[cur.first][cur.second][curDir];
+                path[nextPos.first][nextPos.second][dir].insert(cur);
+                path[nextPos.first][nextPos.second][dir].insert(nextPos);
+                q.push({distance[nextPos.first][nextPos.second][dir], nextPos, dir, path[nextPos.first][nextPos.second][dir]});
+            } else if (distance[nextPos.first][nextPos.second][dir] == distance[cur.first][cur.second][curDir] + 1 + rotate) {
+                for (coord p : path[cur.first][cur.second][curDir]) path[nextPos.first][nextPos.second][dir].insert(p);
+            }
         }
     }
-    cout << "Part 1: " << distance1[1][cols - 2] << endl;
+    for (auto [key, val] : distance[1][cols - 2]) {
+        if (val < part1) {
+            part1 = val;
+            part2 = path[1][cols - 2][key].size();
+        }
+    }
+    cout << "Part 1: " << part1 << endl;
     cout << "Part 2: " << part2 << endl;
     return 0;
 }

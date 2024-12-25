33c7034629ea4221200b34bc097d07a73483f840
Martin Kabanen <martin.kabanen@gmail.com>
2024-12-25 09:26:19 +0200

    2024-25

--- 2024/25.cpp
+++ 2024/25.cpp
@@ -16,38 +16,15 @@ int main() {
         cur.push_back(line);
     }
     schematics.push_back(cur);
-    vector<vector<num>> locks;
-    vector<vector<num>> keys;
-    for (auto schematic : schematics) {
-        if (schematic[0][0] == '#') {
-            vector<num> heights;
-            for (num col = 0; col < schematic[0].length(); ++col) {
-                for (num row = 1; row < schematic.size(); ++row) {
-                    if (schematic[row][col] == '.') {
-                        heights.push_back(row - 1);
-                        break;
-                    }
-                }
-            }
-            locks.push_back(heights);
-        } else {
-            vector<num> heights;
-            for (num col = 0; col < schematic[0].length(); ++col) {
-                for (num row = schematic.size() - 2; row >= 0; --row) {
-                    if (schematic[row][col] == '.') {
-                        heights.push_back(schematic.size() - 2 - row);
-                        break;
-                    }
-                }
-            }
-            keys.push_back(heights);
-        }
-    }
-    for (auto lock : locks) {
-        for (auto key : keys) {
+    for (num i = 0; i < schematics.size(); ++i) {
+        for (num j = i + 1; j < schematics.size(); ++j) {
             bool fits = true;
-            for (num i = 0; i < lock.size(); ++i) {
-                if (lock[i] + key[i] >= 6) fits = false;
+            for (num row = 0; row < schematics[i].size(); ++row) {
+                for (num col = 0; col < schematics[i][0].length(); ++col) {
+                    if (schematics[i][row][col] == '#' && schematics[j][row][col] == '#') {
+                        fits = false;
+                    }
+                }
             }
             if (fits) ++part1;
         }

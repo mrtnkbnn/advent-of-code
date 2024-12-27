d47ad826d74e8d07b49637c2bc1aeb5fb4ec39f7
Martin Kabanen <martin.kabanen@gmail.com>
2024-12-28 00:09:17 +0200

    intcode

--- 2019/07.cpp
+++ 2019/07.cpp
@@ -1,156 +1,40 @@
-#include <bits/stdc++.h>
-using namespace std;
-using num = long long;
+#include "intcode.hpp"
 
-struct intcode {
-    vector<num> program, inputs, outputs;
-    num pointer = 0, inputPointer = 0;
-    bool halted = false;
-
-    intcode(string input) {
-        replace(input.begin(), input.end(), ',', ' ');
-        stringstream ss(input);
-        num n;
-        while (ss >> n) program.push_back(n);
-    }
-
-    void run() {
-        while (true) {
-            num value = program[pointer];
-            num opcode = value % 100;
-            value /= 100;
-            if (opcode == 1) {
-                num parameters = 3;
-                auto modes = parameterModes(parameters, value);
-                num param1 = modes[0] == 0 ? program[program[pointer + 1]] : program[pointer + 1];
-                num param2 = modes[1] == 0 ? program[program[pointer + 2]] : program[pointer + 2];
-                program[program[pointer + 3]] = param1 + param2;
-                pointer += parameters + 1;
-            } else if (opcode == 2) {
-                num parameters = 3;
-                auto modes = parameterModes(parameters, value);
-                num param1 = modes[0] == 0 ? program[program[pointer + 1]] : program[pointer + 1];
-                num param2 = modes[1] == 0 ? program[program[pointer + 2]] : program[pointer + 2];
-                program[program[pointer + 3]] = param1 * param2;
-                pointer += parameters + 1;
-            } else if (opcode == 3) {
-                if (inputPointer >= inputs.size()) {
-                    break;
-                }
-                num parameters = 1;
-                program[program[pointer + 1]] = inputs[inputPointer];
-                ++inputPointer;
-                pointer += parameters + 1;
-            } else if (opcode == 4) {
-                num parameters = 1;
-                auto modes = parameterModes(parameters, value);
-                num param1 = modes[0] == 0 ? program[program[pointer + 1]] : program[pointer + 1];
-                pointer += parameters + 1;
-                outputs.push_back(param1);
-            } else if (opcode == 5) {
-                num parameters = 2;
-                auto modes = parameterModes(parameters, value);
-                num param1 = modes[0] == 0 ? program[program[pointer + 1]] : program[pointer + 1];
-                num param2 = modes[1] == 0 ? program[program[pointer + 2]] : program[pointer + 2];
-                if (param1 != 0) {
-                    pointer = param2;
-                } else {
-                    pointer += parameters + 1;
-                }
-            } else if (opcode == 6) {
-                num parameters = 2;
-                auto modes = parameterModes(parameters, value);
-                num param1 = modes[0] == 0 ? program[program[pointer + 1]] : program[pointer + 1];
-                num param2 = modes[1] == 0 ? program[program[pointer + 2]] : program[pointer + 2];
-                if (param1 == 0) {
-                    pointer = param2;
-                } else {
-                    pointer += parameters + 1;
-                }
-            } else if (opcode == 7) {
-                num parameters = 3;
-                auto modes = parameterModes(parameters, value);
-                num param1 = modes[0] == 0 ? program[program[pointer + 1]] : program[pointer + 1];
-                num param2 = modes[1] == 0 ? program[program[pointer + 2]] : program[pointer + 2];
-                if (param1 < param2) {
-                    program[program[pointer + 3]] = 1;
-                } else {
-                    program[program[pointer + 3]] = 0;
-                }
-                pointer += parameters + 1;
-            } else if (opcode == 8) {
-                num parameters = 3;
-                auto modes = parameterModes(parameters, value);
-                num param1 = modes[0] == 0 ? program[program[pointer + 1]] : program[pointer + 1];
-                num param2 = modes[1] == 0 ? program[program[pointer + 2]] : program[pointer + 2];
-                if (param1 == param2) {
-                    program[program[pointer + 3]] = 1;
-                } else {
-                    program[program[pointer + 3]] = 0;
-                }
-                pointer += parameters + 1;
-            } else if (opcode == 99) {
-                halted = true;
-                break;
-            }
-        }
-    }
-
-
-    private: vector<num> parameterModes(num n, num value) {
-        vector<num> digits;
-        while (value > 0) {
-            digits.push_back(value % 10);
-            value /= 10;
-        }
-        vector<num> result = digits;
-        for (num i = 0; i < (n - digits.size()); ++i) {
-            result.push_back(0);
-        }
-        return result;
-    }
-};
-
-num thruster(vector<num> phase, intcode &program) {
-    vector<intcode> amplifiers(5, program);
+num thruster(vector<num> phase, string line) {
+    vector<intcode> amplifiers(5, intcode(line));
     num prev = 0;
     for (num i = 0; i < phase.size(); ++i) {
-        amplifiers[i].inputs = {phase[i], prev};
-        amplifiers[i].run();
-        prev = amplifiers[i].outputs.back();
+        amplifiers[i].run({phase[i], prev});
+        prev = amplifiers[i].output.back();
     }
     return prev;
 }
 
-
-num feedback(vector<num> phase, intcode &program) {
-    vector<intcode> amplifiers(5, program);
-    for (num i = 0; i < 5; ++i) amplifiers[i].inputs.push_back(phase[i]);
+num feedback(vector<num> phase, string line) {
+    vector<intcode> amplifiers(5, intcode(line));
+    for (num i = 0; i < 5; ++i) amplifiers[i].input.push_back(phase[i]);
     num pointer = 0;
     num prev = 0;
     while (true) {
-        amplifiers[pointer].inputs.push_back(prev);
-        amplifiers[pointer].run();
-        prev = amplifiers[pointer].outputs.back();
+        amplifiers[pointer].run(prev);
+        prev = amplifiers[pointer].output.back();
         if (pointer == 4 && amplifiers[pointer].halted) break;
         pointer = (pointer + 1) % 5;
     }
     return prev;
 }
 
-
 int main() {
     num part1 = LONG_LONG_MIN, part2 = LONG_LONG_MIN;
     string line;
     getline(cin, line);
-    intcode program(line);
     vector<num> phase = {0, 1, 2, 3, 4};
     do {
-        part1 = max(part1, thruster(phase, program));
+        part1 = max(part1, thruster(phase, line));
     } while (next_permutation(phase.begin(), phase.end()));
     phase = {5, 6, 7, 8, 9};
     do {
-        part2 = max(part2, feedback(phase, program));
+        part2 = max(part2, feedback(phase, line));
     } while (next_permutation(phase.begin(), phase.end()));
     cout << "Part 1: " << part1 << endl;
     cout << "Part 2: " << part2 << endl;
---
+++ 2019/intcode.hpp
@@ -0,0 +1,153 @@
+#include <bits/stdc++.h>
+using namespace std;
+using num = long long;
+
+struct intcode {
+    vector<num> memory, input, output;
+    num pointer = 0, inputPointer = 0, relativeBase = 0;
+    bool halted = false;
+
+    intcode(string program) {
+        replace(program.begin(), program.end(), ',', ' ');
+        stringstream ss(program);
+        num n;
+        while (ss >> n) memory.push_back(n);
+    }
+
+    void extend(num pos) {
+        num limit = pos - memory.size() + 1;
+        for (num i = 0; i < limit; ++i) memory.push_back(0);
+    }
+
+    num read(num pos) {
+        extend(pos);
+        return memory[pos];
+    }
+
+    void write(num pos, num val) {
+        extend(pos);
+        memory[pos] = val;
+    }
+
+    num param(num pos, num mode) {
+        return read(addr(pos, mode));
+    }
+
+    num addr(num pos, num mode) {
+        if (mode == 0) {
+            return read(pos);
+        } else if (mode == 1) {
+            return pos;
+        } else if (mode == 2) {
+            return relativeBase + read(pos);
+        } else {
+            throw invalid_argument("invalid mode");
+        }
+    }
+
+    void run(num val) {
+        input.push_back(val);
+        run();
+    }
+
+    void run(vector<num> vals) {
+        for (num val : vals) input.push_back(val);
+        run();
+    }
+
+    void run() {
+        if (halted) throw invalid_argument("halted");
+        while (true) {
+            num value = read(pointer);
+            num opcode = value % 100;
+            if (opcode == 1) {
+                auto mode = modes(3, value);
+                num param1 = param(pointer + 1, mode[0]);
+                num param2 = param(pointer + 2, mode[1]);
+                write(addr(pointer + 3, mode[2]), param1 + param2);
+                pointer += 4;
+            } else if (opcode == 2) {
+                auto mode = modes(3, value);
+                num param1 = param(pointer + 1, mode[0]);
+                num param2 = param(pointer + 2, mode[1]);
+                write(addr(pointer + 3, mode[2]), param1 * param2);
+                pointer += 4;
+            } else if (opcode == 3) {
+                if (inputPointer >= input.size()) {
+                    return;
+                }
+                auto mode = modes(1, value);
+                write(addr(pointer + 1, mode[0]), input[inputPointer]);
+                ++inputPointer;
+                pointer += 2;
+            } else if (opcode == 4) {
+                auto mode = modes(1, value);
+                num param1 = param(pointer + 1, mode[0]);
+                output.push_back(param1);
+                pointer += 2;
+            } else if (opcode == 5) {
+                auto mode = modes(2, value);
+                num param1 = param(pointer + 1, mode[0]);
+                num param2 = param(pointer + 2, mode[1]);
+                if (param1 != 0) {
+                    pointer = param2;
+                } else {
+                    pointer += 3;
+                }
+            } else if (opcode == 6) {
+                auto mode = modes(2, value);
+                num param1 = param(pointer + 1, mode[0]);
+                num param2 = param(pointer + 2, mode[1]);
+                if (param1 == 0) {
+                    pointer = param2;
+                } else {
+                    pointer += 3;
+                }
+            } else if (opcode == 7) {
+                auto mode = modes(3, value);
+                num param1 = param(pointer + 1, mode[0]);
+                num param2 = param(pointer + 2, mode[1]);
+                if (param1 < param2) {
+                    write(addr(pointer + 3, mode[2]), 1);
+                } else {
+                    write(addr(pointer + 3, mode[2]), 0);
+                }
+                pointer += 4;
+            } else if (opcode == 8) {
+                auto mode = modes(3, value);
+                num param1 = param(pointer + 1, mode[0]);
+                num param2 = param(pointer + 2, mode[1]);
+                if (param1 == param2) {
+                    write(addr(pointer + 3, mode[2]), 1);
+                } else {
+                    write(addr(pointer + 3, mode[2]), 0);
+                }
+                pointer += 4;
+            } else if (opcode == 9) {
+                auto mode = modes(1, value);
+                num param1 = param(pointer + 1, mode[0]);
+                relativeBase += param1;
+                pointer += 2;
+            } else if (opcode == 99) {
+                halted = true;
+                return;
+            } else {
+                throw invalid_argument("invalid opcode");
+            }
+        }
+    }
+
+    private: vector<num> modes(num n, num value) {
+        value /= 100;
+        vector<num> digits;
+        while (value > 0) {
+            digits.push_back(value % 10);
+            value /= 10;
+        }
+        vector<num> result = digits;
+        for (num i = 0; i < (n - digits.size()); ++i) {
+            result.push_back(0);
+        }
+        return result;
+    }
+};
